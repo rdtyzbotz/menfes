e301e25126eccbf193ad47a8dee96faa567ffb35
rdtyzbotz <113617871+rdtyzbotz@users.noreply.github.com>
2022-10-17 20:39:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285730794089']
+global.ownernomer = "6285730794089"
+global.premium = ['6285730794089']
+global.packname = 'GilzMaker'
+global.author = 'GilzzAza'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
